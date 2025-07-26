605cf6578cb5c73560c2932d3a1b624d8dfba40c
Rodrigo Alves <f.rodrigoalves12@gmail.com>
2025-07-26 04:42:18 +0000

    quero que essa sessão se torna a land page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,11 @@
 import { LocalizationProvider } from '@/components/localization-provider';
 import Hero from '@/components/sections/hero';
-import About from '@/components/sections/about';
-import Technologies from '@/components/sections/technologies';
-import Projects from '@/components/sections/projects';
-import Gpts from '@/components/sections/gpts';
-import Certifications from '@/components/sections/certifications';
-import Partners from '@/components/sections/partners';
-import Footer from '@/components/footer';
-import FloatingButtons from '@/components/floating-buttons';
 
 export default function Home() {
   return (
     <LocalizationProvider>
       <div className="flex min-h-screen flex-col">
         <Hero />
-        <main className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <About />
-          <Technologies />
-          <Projects />
-          <Gpts />
-          <Certifications />
-          <Partners />
-        </main>
-        <Footer />
-        <FloatingButtons />
       </div>
     </LocalizationProvider>
   );
