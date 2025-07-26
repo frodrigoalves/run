55d2c0d918c8f2a14a24d46de9d2c22572f6c9fa
Rodrigo Alves <f.rodrigoalves12@gmail.com>
2025-07-26 07:14:49 +0000

    Acho que da para redistribuir melhor

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -60,13 +60,13 @@ export default function Hero() {
           isFeatured={featuredMatrixIndex === 3}
         />
       </div>
-       <div className="absolute bottom-1/3 left-16 opacity-0 animate-fade-in-delay-2 hidden md:block">
+       <div className="absolute bottom-2/3 left-1/4 opacity-0 animate-fade-in-delay-2 hidden md:block">
         <MatrixEffect 
           strings={allMatrixStrings[2]}
           isFeatured={featuredMatrixIndex === 2}
         />
       </div>
-       <div className="absolute top-1/4 right-24 opacity-0 animate-fade-in-delay-4 hidden md:block">
+       <div className="absolute top-3/4 right-1/4 opacity-0 animate-fade-in-delay-4 hidden md:block">
         <MatrixEffect 
           strings={allMatrixStrings[4]}
           isFeatured={featuredMatrixIndex === 4}
