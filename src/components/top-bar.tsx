118a94aee74176749c8cdbd5003eda3fdfeaa8d0
Rodrigo Alves <f.rodrigoalves12@gmail.com>
2025-07-27 02:32:49 +0000

    movimente os icones de idioma e temas mais um pouco para o centro d tela

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -8,7 +8,7 @@ interface TopBarProps {
 
 export default function TopBar({ showNav }: TopBarProps) {
     return (
-        <div className="fixed top-0 left-0 right-0 z-50 glass-effect p-2 flex items-center justify-between">
+        <div className="fixed top-0 left-0 right-0 z-50 glass-effect p-2 pr-4 flex items-center justify-between">
             <div className="w-1/3" />
             <div className="w-1/3 flex justify-center">
                 {showNav && <Header />}
