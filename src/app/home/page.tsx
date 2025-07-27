0ba69ac36bc53b19cb44efe04fa2a56e140aacf8
Rodrigo Alves <f.rodrigoalves12@gmail.com>
2025-07-27 05:14:15 +0000

    incorpore esse  chat, retire   o antigo typebot e insira esse  no outro

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -10,7 +10,25 @@ import { Separator } from '@/components/ui/separator';
 import Partners from '@/components/sections/partners';
 import { HeroAnimationProvider } from '@/contexts/hero-animation-context';
 import TopBar from '@/components/top-bar';
-import { TypebotEmbed } from '@/components/typebot-embed';
+import { Bubble } from '@typebot.io/nextjs';
+
+
+function PortfolioChatbot() {
+  return (
+    <Bubble
+      typebot="rodrigoalves"
+      theme={{
+        placement: 'left',
+        button: {
+          backgroundColor: '#FFFFFF',
+          customIconSrc:
+            "data:image/svg+xml;utf8,<svg fill='%234A8BB2' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><path d='M0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zM188.3 147.1c-7.6 4.2-12.3 12.3-12.3 20.9V344c0 8.7 4.7 16.7 12.3 20.9s16.8 4.1 24.3-.5l144-88c7.1-4.4 11.5-12.1 11.5-20.5s-4.4-16.1-11.5-20.5l-144-88c-7.4-4.5-16.7-4.7-24.3-.5z'/></svg>",
+          size: 'medium',
+        },
+      }}
+    />
+  );
+}
 
 
 function HomePageContent() {
@@ -42,7 +60,7 @@ function HomePageContent() {
       </main>
 
       <Footer />
-      <TypebotEmbed />
+      <PortfolioChatbot />
     </div>
   );
 }
