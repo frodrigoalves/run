cbae8786d4f8d656ce2cdf1de08fff7fcf7fed5a
Rodrigo Alves <f.rodrigoalves12@gmail.com>
2025-07-27 02:22:45 +0000

    colada no topo, alinhe os itens como se foss um menu  minimalista e disc

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -8,7 +8,7 @@ interface TopBarProps {
 
 export default function TopBar({ showNav }: TopBarProps) {
     return (
-        <div className="fixed top-4 left-4 right-4 z-50 glass-effect rounded-full p-2 flex items-center justify-between">
+        <div className="fixed top-0 left-0 right-0 z-50 glass-effect p-2 flex items-center justify-between">
             <div className="w-1/3 flex justify-start">
                  <GlobalControls />
             </div>
